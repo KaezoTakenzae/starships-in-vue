2a0d85d7e422a9f0c1c876a900f9aac856c0aa3d
Kyle Moxham <kyle.moxham@built.co.uk>
2021-05-25 13:09:08 +0100

    Create Header component

---
+++ src/components/header/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+      <button v-on:click="scrollTop">Home</button>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  methods: {
+    scrollTop: function() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  padding: 10px 20px;
+  background-color: #373b3f;
+  color: #fff;
+  height: 45px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+header button {
+    color: #fff;
+    text-decoration: underline;
+    display: inline-block;
+    margin-top: 4px;
+    background-color: transparent;
+    border-width: 0;
+}
+</style>
